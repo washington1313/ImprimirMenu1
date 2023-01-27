ab024e8f3e88022fb3d5371b93c9209e1ddab673
Washinfron Fernandes <wmfmg@hotmail.com>
2023-01-27 15:54:04 -0300

    Projeto Finalizado

---
+++ ImprimirMenu1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ImprimirMenu1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome:");
+            string nomeInformado = Console.ReadLine();
+            Console.Clear();
+            menuFinal(nomeInformado);
+
+            Console.ReadKey();
+        }
+
+        static void menuFinal(string nomeUsuario)
+        {
+            Console.ForegroundColor= ConsoleColor.DarkRed;
+            Console.WriteLine("########## MENU ##########");
+            Console.WriteLine("      " + nomeUsuario + " ");
+            Console.WriteLine("##########################");
+            Console.ResetColor();
+        }
+    }
+
+}
